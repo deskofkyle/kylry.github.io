5a794947b7cffe3f871451d7d2d4d588c4d6407a
Kyle Ryan <kylrya@gmail.com>
2024-01-10 16:37:53 -0800

    Change some spacing

--- aura-box-usa-dot-com/script.js
+++ aura-box-usa-dot-com/script.js
@@ -6,13 +6,13 @@ window.addEventListener('scroll', function() {
         var scrollFraction = scrollY / windowHeight;
 
         // Change image based on scroll position
-        if (scrollFraction < 1) {
+        if (scrollFraction < 0.7) {
             // Scroll is between 0% and 100%
             image.src = 'box-1.png';
-        } else if (scrollFraction < 2) {
+        } else if (scrollFraction < 1.7) {
             // Scroll is between 101% and 200%
             image.src = 'box-2.png';
-        } else if (scrollFraction > 3) {
+        } else if (scrollFraction > 2.7) {
             // Scroll is between 201% and 300%
             image.src = 'box-3.png';
         } // Add more conditions as needed for more images
